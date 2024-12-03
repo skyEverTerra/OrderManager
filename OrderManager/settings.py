002ea7675614900e92d403c0d8244715684219cc
skyEverTerra <everex001@gmail.com>
2024-12-02 18:32:14 -0600

    despliege 1.4.4

--- OrderManager/settings.py
+++ OrderManager/settings.py
@@ -58,6 +58,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Añadir esta línea
 ]
 
 ROOT_URLCONF = "OrderManager.urls"
